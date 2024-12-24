83ff0d2220e7ca8f45ae47bd6687dda39b825888
Ronald <rborjaroman@gmail.com>
2024-12-23 22:03:01 -0500

    api development

---
+++ schemes/house_scheme.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, ConfigDict
+
+class HouseRequestModel(BaseModel):
+    square_footage: float
+    num_bedrooms: int
+    num_bathrooms: int
+    year_built: int
+    lot_size: float 
+    garage_size: int
+    neighborhood_quality: int 
+    model_config: ConfigDict = {
+    }
+
+
+class HouseResponseModel(BaseModel): 
+    square_footage: float
+    num_bedrooms: int
+    num_bathrooms: int
+    year_built: int
+    lot_size: float 
+    garage_size: int
+    neighborhood_quality: int 
+    model_config = ConfigDict(from_attributes=True)
